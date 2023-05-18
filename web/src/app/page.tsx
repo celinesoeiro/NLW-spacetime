bfdc1824a85887403b76dbc8df1e132852403682
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2023-05-18 14:56:24 -0300

    chore: add some styles with tailwind

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="text-lg text-red-700 font-bold">Hello, world!</h1>
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+      <h1 className="text-4xl font-bold">Your time capsule</h1>
+    </div>
+  )
 }
