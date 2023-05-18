89f31485988cab934939c4e4e30110dd4d65fa2e
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2023-05-18 15:29:03 -0300

    chore: fix prettier problems

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className="bg-zinc-950 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 text-center font-bold text-5xl">
+    <View className="flex-1 items-center justify-center bg-zinc-950">
+      <Text className="text-center text-5xl font-bold text-zinc-50">
         Your time capsule
       </Text>
       <StatusBar style="light" translucent />
