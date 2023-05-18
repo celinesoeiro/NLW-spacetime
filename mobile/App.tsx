cda74f81dbeb7c6fa5076d362d83dbb81a42785a
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2023-05-18 15:27:29 -0300

    chore: eslint + prettier config

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className='bg-zinc-950 flex-1 items-center justify-center'>
-      <Text className='text-zinc-50 text-center font-bold text-5xl'>Your time capsule</Text>
+    <View className="bg-zinc-950 flex-1 items-center justify-center">
+      <Text className="text-zinc-50 text-center font-bold text-5xl">
+        Your time capsule
+      </Text>
       <StatusBar style="light" translucent />
     </View>
-  );
+  )
 }
