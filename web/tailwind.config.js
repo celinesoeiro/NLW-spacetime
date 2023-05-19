782988ba79df35fcc2e809ac70305d3e5a902db4
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2023-05-19 10:43:49 -0300

    feat: finish layout

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -21,6 +21,9 @@ module.exports = {
       backgroundSize: {
         stripes: '100% 8px',
       },
+      fontSize: {
+        '5xl': '2.5rem',
+      },
       colors: {
         gray: {
           50: '#eaeaea',
