932b0cda90336796bc232894260e8bfaf57489b5
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2023-05-19 09:35:26 -0300

    feat: creating grid layout with initial style

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,26 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Your time capsule</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/** LEFT SECTION */}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16">
+        {/** Blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        {/** Stripes */}
+        <div className="absolute bottom-0 right-2 top-0 w-2  bg-stripes"></div>
+      </div>
+
+      {/** RIGHT SECTION */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            You haven&apos;t registered any memories yet, start{' '}
+            <a href="" className="cursor-pointer underline hover:text-gray-50">
+              creating now
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
