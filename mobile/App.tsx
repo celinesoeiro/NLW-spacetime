da9c746684adc005e0fdcca6b58f13c94fbee5d4
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2023-05-20 09:26:04 -0300

    chore: Add initial layout + handle SVG

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground } from 'react-native'
 
 import {
   useFonts,
@@ -7,6 +7,12 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { styled } from 'nativewind'
+
+import Stripes from './assets/stripes.svg'
+import blurBg from './assets/luz.png'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -21,11 +27,14 @@ export default function App() {
   }
 
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="text-center font-title text-5xl font-bold text-gray-50">
-        Your time capsule
-      </Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
