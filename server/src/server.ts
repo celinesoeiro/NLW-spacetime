31167701252c0c40ef4e95e4e53f6d1a92fb8469
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2023-05-20 11:14:42 -0300

    feat: add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
+app.register(cors, {
+  origin: ['http://localhost:3000'],
+})
 app.register(memoriesRoutes)
 
 app
