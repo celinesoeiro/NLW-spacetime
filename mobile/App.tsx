f7a739901d67720577cdd33d0192851c046754c8
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2023-05-20 10:15:54 -0300

    chore: layout pt 2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground } from 'react-native'
+import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
 
 import {
   useFonts,
@@ -10,6 +10,7 @@ import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 import { styled } from 'nativewind'
 
 import Stripes from './assets/stripes.svg'
+import Logo from './assets/nlw-logo.svg'
 import blurBg from './assets/luz.png'
 
 const StyledStripes = styled(Stripes)
@@ -29,10 +30,36 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBg}
-      className="relative flex-1 items-center bg-gray-900"
+      className="relative flex-1 items-center bg-gray-900 px-12 py-10"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <Logo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Your time capsule
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Collect memorable moments from your journey and share (if you like)
+            with the world!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-3"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Register a memory
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Made with 💜 by Celine Soeiro at Rocketseat&apos;s NLW
+      </Text>
 
       <StatusBar style="light" translucent />
     </ImageBackground>
