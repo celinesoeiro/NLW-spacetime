c0081745e5f2639151aeeceb7fb2bd382dbca653
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2023-05-21 13:18:35 -0300

    chore: add JWT token

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,10 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
@@ -7,7 +12,12 @@ const app = fastify()
 app.register(cors, {
   origin: ['http://localhost:3000'],
 })
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
