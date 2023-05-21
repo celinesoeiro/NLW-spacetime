516e3c8f9105c41ba7ebf2589acb0fd4c9131a27
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2023-05-21 17:21:01 -0300

    chore: save token on cookies

---
+++ web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,24 @@
+import { api } from '@/lib/api'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+
+  const code = searchParams.get('code')
+
+  const reisterResponse = await api.post('/register', {
+    code,
+  })
+
+  const { token } = reisterResponse.data
+
+  const redirectUrl = new URL('/', request.url)
+
+  const cookiesExpiresInSecons = 60 * 60 * 24 * 30
+
+  return NextResponse.redirect(redirectUrl, {
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookiesExpiresInSecons}`,
+    },
+  })
+}
