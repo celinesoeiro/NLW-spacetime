4445103d6be39a9c255e8a38c19304472030cbe5
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2023-05-21 21:57:14 -0300

    chore: file upload + static serving files

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,13 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+
+import { resolve } from 'node:path'
 
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
 
@@ -15,9 +19,15 @@ app.register(cors, {
 app.register(jwt, {
   secret: 'spacetime',
 })
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads/',
+})
 
 app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
